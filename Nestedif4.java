e1a54246198b83a2a6ff4d4fef9c88a5178ff82b
Tushar-435 <122785295+Tushar-435@users.noreply.github.com>
2023-01-24 17:22:28 +0530

    Add files via upload

---
+++ Nestedif4.java
@@ -0,0 +1,15 @@
+class Nestedif4{
+public static void main(String[] arg) {
+int num1=2;
+int num2=3;
+if(num1 == 2){
+if (num2 == 5){
+System.out.println("Wrong statement");
+}
+else{
+System.out.println("Not matched");
+}
+}
+else{
+System.out.println("1st condition not satisfied");
+}}}
