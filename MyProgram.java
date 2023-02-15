3d768cb966624a2092c06b9eb827ae042fbfaf7d
Tushar-435 <122785295+Tushar-435@users.noreply.github.com>
2023-02-15 17:46:59 +0530

    Add files via upload

---
+++ MyProgram.java
@@ -0,0 +1,15 @@
+class DisplayMessage{
+	  void printMessage(){
+	   System.out.println("Hello My Message");
+	}
+	}  
+public class  MyProgram extends DisplayMessage
+{
+  public static void main(String arg[]){
+    MyProgram program=new MyProgram();
+    program.printMessage();
+ }
+}
+
+	
+
